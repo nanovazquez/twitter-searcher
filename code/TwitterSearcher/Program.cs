c7a86346fe218b378bd642c3b406493716191372
nanovazquez <marianodvazquez@gmail.com>
2015-11-04 09:52:36 -0800

    Apply fixes to code

---
+++ code/TwitterSearcher/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterUtils;
+
+namespace TwitterSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Twitter Searcher");
+            Console.WriteLine("-----------------");
+
+            var searchQueries = new Dictionary<string, string>
+            {
+                //{ "cerveza", "cerveza OR beer OR cerveja OR bière" },
+                { "vino", "vino OR wine OR vinho OR vin" },
+                { "whisky", "whisky OR whiskey OR uísque" },
+                { "vodka", "vodka" },
+                { "fernet", "fernet" }
+            };
+            var geolocations = new[]
+            {
+                "-36.070559,-65.132268,2500km",
+                "5.307503,-56.706703,3500km",
+                "40.323962,-74.756002,2300km",
+                "46.773343,-122.846604,2200km",
+                "66.433255,-151.966204,1000km",
+                "22.075772,-102.808852,2000km",
+                "-36.070559,-65.132268,2500km",
+                "5.307503,-56.706703,3500km"
+            };
+            var numberOfTweets = 10000;
+
+            foreach(var searchQuery in searchQueries)
+            {
+                Console.WriteLine("Searching for: {0}", searchQuery);
+                foreach(var geoLocation in geolocations)
+                {
+                    Console.WriteLine("In location: {0}", geoLocation);
+                    var searcher = new TwitterApiSearcher();
+                    var tweets = searcher.GetTweetsAsync(searchQuery.Value, geoLocation, numberOfTweets).Result;
+
+                    var outputFilePath = string.Format("{0}.txt", searchQuery.Key);
+                    File.AppendAllLines(outputFilePath, tweets.ToArray());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Done. Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ code/TwitterSearcher/TwitterApiSearcher.cs
@@ -0,0 +1,115 @@
+﻿using LinqToTwitter;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TwitterUtils
+{
+    public class TwitterApiSearcher
+    {
+        private static readonly string TwitterConsumerKeyName = "TwitterApiConsumerKey";
+
+        private static readonly string TwitterConsumerSecretName = "TwitterApiConsumerSecret";
+
+        private static readonly TwitterContext Context = null;
+
+        // Static constructor is called at most one time, before any
+        // instance constructor is invoked or member is accessed.
+        static TwitterApiSearcher()
+        {
+            Context = InitializeTwitterContextAsync().Result;
+        }
+
+        public async Task<IEnumerable<string>> GetTweetsAsync(string searchQuery, string geolocationInfo, int limit = 100, string searchLanguages = "es")
+        {
+            var windowInMinutes = 15;
+            var requestsLimitInWindow = 450;
+            var tweetsReceived = 0;
+            var numberOfRequests = 0;
+            var startTime = DateTime.Now;
+            var result = new List<string>();
+            long maxId = 0;
+
+            while (tweetsReceived < limit && maxId >= 0)
+            {
+                if (numberOfRequests < requestsLimitInWindow)
+                {
+                    try
+                    {
+                        var searchResponse = 
+                            await Context.Search.Where(
+                                s => s.Type == SearchType.Search
+                                        && s.Count == 100
+                                        //&& s.SearchLanguage == searchLanguages
+                                        && s.GeoCode == geolocationInfo
+                                        && s.MaxID == (ulong)maxId
+                                        && s.Query == searchQuery)
+                            .SingleOrDefaultAsync()
+                            .ConfigureAwait(false);
+
+                        numberOfRequests++;
+                        if (searchResponse != null && searchResponse.Statuses != null)
+                        {
+                            if (searchResponse.SearchMetaData != null && searchResponse.SearchMetaData.NextResults != null)
+                            {
+                                var nextMaxId = searchResponse.SearchMetaData.NextResults.Split(new string[] { "max_id=" }, StringSplitOptions.RemoveEmptyEntries)[1].Split('&')[0];
+                                maxId = Convert.ToInt64(nextMaxId);
+                            }
+                            else
+                            {
+                                maxId = -1;
+                            }
+
+                            var tweets = searchResponse.Statuses.Select(t => string.Format("\"{0}\",{1},{2},{3}", t.Text, t.User.FriendsCount, t.Place.FullName, geolocationInfo));
+                            tweetsReceived += tweets.Count();
+                            result.AddRange(tweets);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        if ((e.Message != null && e.Message.Contains("Rate limit exceeded"))
+                             || (e.InnerException != null && e.InnerException.Message.Contains("Rate limit exceeded")))
+                        {
+                            numberOfRequests = requestsLimitInWindow;
+                        }else
+                        {
+                            Console.WriteLine("Unknown error: {0}", e.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    // Request limit exceeded
+                    // Wait until request quota is restarted (max 15 minutes)
+                    var minutesToWait = windowInMinutes - (DateTime.Now - startTime).Minutes + 1;
+                    Console.WriteLine("Request limit exceeded. Waiting {0} minutes (at {1})", minutesToWait, DateTime.Now.ToShortTimeString());
+                    Task.Delay(TimeSpan.FromMinutes(minutesToWait)).Wait();
+                    numberOfRequests = 0;
+                    startTime = DateTime.Now;
+                }
+            }
+
+            return result;
+        }
+
+        private static async Task<TwitterContext> InitializeTwitterContextAsync()
+        {
+            var auth = new ApplicationOnlyAuthorizer
+            {
+                CredentialStore = new SingleUserInMemoryCredentialStore
+                {
+                    ConsumerKey = ConfigurationManager.AppSettings[TwitterConsumerKeyName],
+                    ConsumerSecret = ConfigurationManager.AppSettings[TwitterConsumerSecretName],
+                }
+            };
+
+            await auth.AuthorizeAsync().ConfigureAwait(false);
+
+            return new TwitterContext(auth);
+        }
+    }
+}
